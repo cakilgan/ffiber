a953f621d7e515f54d2eb527e78ebfd3b38cb52a
cakilgan <cakilganhan@gmail.com>
2026-04-17 23:44:04 +0300

    make api free
    hided schedule to ff::detail and made api easy to use by free functions

--- ffiber.cpp
+++ ffiber.cpp
@@ -1,33 +1,29 @@
 #include "ffiber.h"
+
 namespace ff {
-thread_local fiber *g_current_fiber = nullptr;
-thread_local scheduler *g_current_scheduler = nullptr;
+
+thread_local fiber *globals::current_fiber = nullptr;
+thread_local detail::scheduler *globals::current_scheduler = nullptr;
 
 void context::make(context *ctx, void *stack_mem, uint32_t stack_size,
                    void (*func)()) {
     uint64_t *stack = (uint64_t *)((char *)stack_mem + stack_size);
-
     *(--stack) = 0;
-
     *(--stack) = (uint64_t)func;
-
     for (int i = 0; i < 6; ++i)
         *(--stack) = 0;
-
     ctx->rsp = stack;
 }
 
-void scheduler::step() {
-    g_current_scheduler = this;
+void detail::scheduler::step() {
+    globals::current_scheduler = this;
 
     for (size_t i = 0; i < _waiting.size();) {
-        size_t ix = _waiting[i];
+        int ix = _waiting[i];
         fiber &f = _fibers[ix];
-
         if (f.waiting_for && f.waiting_for->value.load() <= 0) {
             f.state = fiber::ready;
             _yielded.push_back(ix);
-
             _waiting[i] = _waiting.back();
             _waiting.pop_back();
         } else {
@@ -37,83 +33,65 @@ void scheduler::step() {
 
     std::vector<int> to_run;
     to_run.swap(_yielded);
-
-    for (size_t ix : to_run) {
+    for (int ix : to_run) {
         fiber &f = _fibers[ix];
         f.state = fiber::running;
-        g_current_fiber = &f;
-
+        globals::current_fiber = &f;
         switch_context(&_context.rsp, f.cx.rsp);
     }
 
     while (!_jobs.empty() && !_free_fibers.empty()) {
         fiber *idle_f = &_fibers[_free_fibers.back()];
         _free_fibers.pop_back();
-
-        if (!idle_f)
-            break;
-
         idle_f->current_job = _jobs.front();
         _jobs.pop();
         idle_f->state = fiber::running;
-
-        g_current_fiber = idle_f;
-        g_current_scheduler = this;
-
+        globals::current_fiber = idle_f;
         switch_context(&_context.rsp, idle_f->cx.rsp);
     }
-    g_current_scheduler = nullptr;
 }
-void yield() {
-    fiber *self = g_current_fiber;
-    scheduler *sched = g_current_scheduler;
 
+void yield() {
+    fiber *self = globals::current_fiber;
+    detail::scheduler *sched = globals::current_scheduler;
     if (!self || !sched)
         return;
 
     self->state = fiber::ready;
     sched->add_yield(self->ix);
-
     switch_context(&self->cx.rsp, sched->get_context().rsp);
-
     self->state = fiber::running;
 }
-void wait(counter *c) {
-    fiber *self = g_current_fiber;
-    scheduler *sched = g_current_scheduler;
 
+void wait(counter *c) {
+    fiber *self = globals::current_fiber;
+    detail::scheduler *sched = globals::current_scheduler;
     if (!self || !sched)
         return;
-
     if (c->value.load() <= 0)
         return;
 
     self->waiting_for = c;
     self->state = fiber::suspended;
     sched->add_waiting(self->ix);
-
     switch_context(&self->cx.rsp, sched->get_context().rsp);
-
     self->waiting_for = nullptr;
     self->state = fiber::running;
 }
 
 void fiber_entry_point() {
     while (true) {
-        fiber *self = g_current_fiber;
-        scheduler *sched = g_current_scheduler;
-
+        fiber *self = globals::current_fiber;
+        detail::scheduler *sched = globals::current_scheduler;
         if (self->current_job.func) {
             self->current_job.func(self->current_job.data);
-
             self->current_job = {nullptr, nullptr};
             self->waiting_for = nullptr;
             self->state = fiber::idle;
-
             sched->add_free_fiber(self->ix);
         }
-
         switch_context(&self->cx.rsp, sched->get_context().rsp);
     }
 }
+
 } // namespace ff
--- ffiber.h
+++ ffiber.h
@@ -11,7 +11,6 @@ extern "C" {
 void switch_context(void **old_stack, void *new_stack);
 }
 
-// namespace
 namespace ff {
 
 namespace util {
@@ -28,11 +27,13 @@ struct job {
     void (*func)(void *);
     void *data;
 };
+
 struct context {
     void *rsp;
     static void make(context *cx, void *stack_memory, uint32_t stack_size,
                      void (*func)());
 };
+
 struct fiber {
     context cx;
     job current_job;
@@ -43,21 +44,25 @@ struct fiber {
 
 extern void fiber_entry_point();
 
+// --- internal scheduler (not part of the public API) ---
+namespace detail {
+
 struct scheduler {
     template <typename Allocator = util::allocator_malloc>
     void init(size_t fiber_count, size_t stack_size) {
         _fibers.resize(fiber_count);
-        for (size_t i{0}; i < fiber_count; i++) {
+        for (size_t i = 0; i < fiber_count; i++) {
             _fibers[i] =
                 fiber{context{nullptr}, job{nullptr, nullptr}, fiber::idle};
             context::make(&_fibers[i].cx, Allocator::allocate(stack_size),
                           stack_size, fiber_entry_point);
-            _fibers[i].ix = i;
-            _free_fibers.push_back(i);
+            _fibers[i].ix = (int)i;
+            _free_fibers.push_back((int)i);
         }
     }
 
     void kick(job j) { _jobs.push(j); }
+
     void kickntrack(job j, counter *c) {
         c->value.fetch_add(1);
         kick(j);
@@ -66,7 +71,6 @@ struct scheduler {
     void step();
 
     context get_context() { return _context; }
-
     void add_yield(int ix) { _yielded.push_back(ix); }
     void add_waiting(int ix) { _waiting.push_back(ix); }
     void add_free_fiber(int ix) { _free_fibers.push_back(ix); }
@@ -74,19 +78,39 @@ struct scheduler {
   private:
     context _context;
     std::vector<fiber> _fibers;
-
     std::vector<int> _yielded;
     std::vector<int> _waiting;
     std::vector<int> _free_fibers;
-
     std::queue<job> _jobs;
 };
+
+} // namespace detail
+
+namespace globals {
+extern thread_local fiber *current_fiber;
+extern thread_local detail::scheduler *current_scheduler;
+} // namespace globals
+
+// --- public free-function API ---
+
+template <typename Allocator = util::allocator_malloc>
+void init(size_t fiber_count, size_t stack_size) {
+    if (!globals::current_scheduler)
+        globals::current_scheduler = new detail::scheduler();
+    globals::current_scheduler->init<Allocator>(fiber_count, stack_size);
+}
+
+inline void kick(job j) { globals::current_scheduler->kick(j); }
+
+inline void kickntrack(job j, counter *c) {
+    globals::current_scheduler->kickntrack(j, c);
+}
+
+inline void step() { globals::current_scheduler->step(); }
 
 void yield();
 void wait(counter *c);
 
-extern thread_local fiber *g_current_fiber;
-extern thread_local scheduler *g_current_scheduler;
-
 } // namespace ff
+
 #endif // FFIBER_H
