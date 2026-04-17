eb339cfc3777ffb4f968a1d809d73d4dbd2292ef
cakilgan <cakilganhan@gmail.com>
2026-04-18 01:21:40 +0300

    remove ai comments

--- context_switch.asm
+++ context_switch.asm
@@ -1,10 +1,5 @@
-; switch_context(void** current_stack, void* next_stack)
-; rdi -> eski stack pointer'ın kaydedileceği adres
-; rsi -> yeni stack pointer'ın olduğu adres
-
 global switch_context
 switch_context:
-    ; 1. Mevcut tüm register'ları stack'e yedekle
     push rbp
     push rbx
     push r12
@@ -12,13 +7,10 @@ switch_context:
     push r14
     push r15
 
-    ; 2. Eski stack pointer'ı rdi'nin gösterdiği yere kaydet
     mov [rdi], rsp
 
-    ; 3. İşlemciye "Senin yeni stack'in rsi'daki adrestir" de
     mov rsp, rsi
 
-    ; 4. Yeni stack'ten yedekleri geri yükle
     pop r15
     pop r14
     pop r13
@@ -26,5 +18,4 @@ switch_context:
     pop rbx
     pop rbp
 
-    ; 5. Yeni yerdeki fonksiyona 'merhaba' de
     ret
